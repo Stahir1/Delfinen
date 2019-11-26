e76c27ccd949c25f3944b38a08b37bdb2c080530
tullebob <jimmythangpham@hotmail.dk>
2019-11-26 15:39:50 +0100

    parentclass info

---
+++ src/delfinen/Model/Info.java
@@ -0,0 +1,84 @@
+package delfinen.Model;
+
+/**
+ *
+ * @author Jimmy
+ */
+public class Info {
+
+    private String name;
+    private int age;
+    private String email;
+    private int phoneNumber;
+    private String city;
+    private int zipCode;
+    private String adress;
+
+    public Info(String name, int age, String email, int phoneNumber, String city, int zipCode, String adress) {
+        this.name = name;
+        this.age = age;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.city = city;
+        this.zipCode = zipCode;
+        this.adress = adress;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public int getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(int zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+    
+}
--- src/delfinen/Model/Medlemmer/Medlem.java
+++ src/delfinen/Model/Medlemmer/Medlem.java
@@ -5,10 +5,16 @@
  */
 package delfinen.Model.Medlemmer;
 
+import delfinen.Model.Info;
+
 /**
  *
  * @author Danie
  */
-public class Medlem {
+public class Medlem extends Info {
+    
+    public Medlem(String name, int age, String email, int phoneNumber, String city, int zipCode, String adress) {
+        super(name, age, email, phoneNumber, city, zipCode, adress);
+    }
     
 }
